b81ff8a0b7af7d718443cf1e926f08ec61ba1d93
Jim Horvath <38920027+jhorv@users.noreply.github.com>
2022-06-24 18:05:31 -0400

    tweak formating, fix warnings

--- src/ClaySharp.Tests/BinderFallbackTests.cs
+++ src/ClaySharp.Tests/BinderFallbackTests.cs
@@ -1,221 +1,199 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ClaySharp.Behaviors;
+﻿using ClaySharp.Behaviors;
 using Microsoft.CSharp.RuntimeBinder;
 using NUnit.Framework;
 
-namespace ClaySharp.Tests
+namespace ClaySharp.Tests;
+
+[TestFixture]
+public class BinderFallbackTests
 {
-    [TestFixture]
-    public class BinderFallbackTests
+    class TestMemberBehavior : ClayBehavior
     {
+        static object ReturnSampleOrProceed(Func<object> proceed, string name) => name == "Sample" ? "Data" : proceed();
 
-        class TestMemberBehavior : ClayBehavior
+        public override object InvokeMember(Func<object> proceed, object self, string name, INamedEnumerable<object> args) =>
+            ReturnSampleOrProceed(proceed, name);
+
+        public override object GetMember(Func<object> proceed, object self, string name) =>
+            ReturnSampleOrProceed(proceed, name);
+
+        public override object SetMember(Func<object> proceed, object self, string name, object value) =>
+            ReturnSampleOrProceed(proceed, name);
+    }
+
+    class TestIndexBehavior : ClayBehavior
+    {
+        public override object GetIndex(Func<object> proceed, object self, IEnumerable<object> keys) =>
+            IsIndexZero(keys) ? "Data" : proceed();
+
+        public override object SetIndex(Func<object> proceed, object self, IEnumerable<object> keys, object value) =>
+            IsIndexZero(keys) ? "Data" : proceed();
+
+        static bool IsIndexZero(IEnumerable<object> keys)
         {
-            public override object InvokeMember(Func<object> proceed, object self, string name, INamedEnumerable<object> args)
-            {
-                return name == "Sample" ? "Data" : proceed();
-            }
-            public override object GetMember(Func<object> proceed, object self, string name)
-            {
-                return name == "Sample" ? "Data" : proceed();
-            }
-            public override object SetMember(Func<object> proceed, object self, string name, object value)
-            {
-                return name == "Sample" ? "Data" : proceed();
-            }
+            return keys.Count() == 1
+                && keys.Single().GetType() == typeof(int)
+                && keys.Cast<int>().Single() == 0;
+        }
+    }
+
+    [Test]
+    public void InvokeMemberThrowsFallbackException()
+    {
+        dynamic alpha = new Object();
+        dynamic beta = new Clay(new TestMemberBehavior());
+
+        var ex1 = Assert.Throws<RuntimeBinderException>(() => alpha.Hello1());
+        Assert.That(ex1!.Message, Does.EndWith("does not contain a definition for 'Hello1'"));
+
+        var ex2 = Assert.Throws<RuntimeBinderException>(() => beta.Hello2());
+        Assert.That(ex2!.Message, Does.EndWith("does not contain a definition for 'Hello2'"));
+
+        Assert.That(beta.Sample(), Is.EqualTo("Data"));
+    }
+
+    [Test]
+    public void GetMemberThrowsFallbackException()
+    {
+        dynamic alpha = new Object();
+        dynamic beta = new Clay(new TestMemberBehavior());
+
+        var ex1 = Assert.Throws<RuntimeBinderException>(() => { var hi = alpha.Hello1; });
+        Assert.That(ex1!.Message, Does.EndWith("does not contain a definition for 'Hello1'"));
+
+        var ex2 = Assert.Throws<RuntimeBinderException>(() => { var hi = beta.Hello2; });
+        Assert.That(ex2!.Message, Does.EndWith("does not contain a definition for 'Hello2'"));
+
+        Assert.That(beta.Sample, Is.EqualTo("Data"));
+    }
+
+    [Test]
+    public void SetMemberThrowsFallbackException()
+    {
+        dynamic alpha = new Object();
+        dynamic beta = new Clay(new TestMemberBehavior());
+
+        var ex1 = Assert.Throws<RuntimeBinderException>(() => { alpha.Hello1 = 1; });
+        Assert.That(ex1!.Message, Does.EndWith("does not contain a definition for 'Hello1'"));
+
+        var ex2 = Assert.Throws<RuntimeBinderException>(() => { beta.Hello2 = 2; });
+        Assert.That(ex2!.Message, Does.EndWith("does not contain a definition for 'Hello2'"));
+
+        var x = (beta.Sample = 3);
+        Assert.That(x, Is.EqualTo("Data"));
+    }
+
+
+    [Test]
+    public void GetIndexThrowsFallbackException()
+    {
+        dynamic alpha = new Object();
+        dynamic beta = new Clay(new TestMemberBehavior());
+
+        var ex1 = Assert.Throws<RuntimeBinderException>(() => { var hi = alpha[0]; });
+        Assert.That(ex1!.Message, Does.Match(@"Cannot apply indexing with \[\] to an expression of type .*"));
+
+        var ex2 = Assert.Throws<RuntimeBinderException>(() => { var hi = beta[0]; });
+        Assert.That(ex2!.Message, Does.Match(@"Cannot apply indexing with \[\] to an expression of type .*"));
+    }
+
+
+    [Test]
+    public void SetIndexThrowsFallbackException()
+    {
+        dynamic alpha = new Object();
+        dynamic beta = new Clay(new TestMemberBehavior());
+
+        var ex1 = Assert.Throws<RuntimeBinderException>(() => { alpha[0] = 1; });
+        Assert.That(ex1!.Message, Does.Match(@"Cannot apply indexing with \[\] to an expression of type .*"));
+
+        var ex2 = Assert.Throws<RuntimeBinderException>(() => { beta[0] = 2; });
+        Assert.That(ex2!.Message, Does.Match(@"Cannot apply indexing with \[\] to an expression of type .*"));
+    }
+
+    public interface IAlpha
+    {
+        string Hello();
+        string Foo();
+    }
+
+    public class Alpha
+    {
+        public virtual string Hello()
+        {
+            return "World";
+        }
+    }
+
+    public class AlphaBehavior : ClayBehavior
+    {
+        public override object InvokeMember(Func<object> proceed, object self, string name, INamedEnumerable<object> args)
+        {
+            return proceed() + "-";
+        }
+        public override object InvokeMemberMissing(Func<object> proceed, object self, string name, INamedEnumerable<object> args)
+        {
+            if (name == "Foo")
+                return "Bar";
+            return proceed();
+        }
+    }
+
+    [Test]
+    public void TestInvokePaths()
+    {
+        var dynamically = ClayActivator.CreateInstance<Alpha>(new IClayBehavior[] {
+            new InterfaceProxyBehavior(),
+            new AlphaBehavior()
+        });
+        Alpha statically = dynamically;
+        IAlpha interfacially = dynamically;
+
+        Assert.That(dynamically.Hello(), Is.EqualTo("World-"));
+        Assert.That(statically.Hello(), Is.EqualTo("World-"));
+        Assert.That(interfacially.Hello(), Is.EqualTo("World-"));
+
+        Assert.That(dynamically.Foo(), Is.EqualTo("Bar-"));
+        Assert.That(interfacially.Foo(), Is.EqualTo("Bar-"));
+
+        Assert.Throws<RuntimeBinderException>(() => dynamically.MissingNotHandled());
+    }
+
+
+    public class Beta
+    {
+        public virtual string Hello
+        {
+            get => "World";
+        }
+    }
+
+    public class BetaBehavior : ClayBehavior
+    {
+        public override object GetMember(Func<object> proceed, object self, string name)
+        {
+            return proceed() + "-";
         }
 
-        class TestIndexBehavior : ClayBehavior
+        public override object GetMemberMissing(Func<object> proceed, object self, string name)
         {
-            public override object GetIndex(Func<object> proceed, object self, IEnumerable<object> keys)
-            {
-                return IsIndexZero(keys) ? "Data" : proceed();
-            }
-
-            public override object SetIndex(Func<object> proceed, object self, IEnumerable<object> keys, object value)
-            {
-                return IsIndexZero(keys) ? "Data" : proceed();
-            }
-
-            private static bool IsIndexZero(IEnumerable<object> keys)
-            {
-                return keys.Count() == 1
-                    && keys.Single().GetType() == typeof(int)
-                    && keys.Cast<int>().Single() == 0;
-            }
+            if (name == "Foo")
+                return "Bar";
+            return proceed();
         }
+    }
 
+    [Test]
+    public void TestGetPaths()
+    {
+        var dynamically = ClayActivator.CreateInstance<Beta>(new[] { new BetaBehavior() });
+        Beta statically = dynamically;
 
-        [Test]
-        public void InvokeMemberThrowsFallbackException()
-        {
-            dynamic alpha = new Object();
-            dynamic beta = new Clay(new TestMemberBehavior());
+        Assert.That(dynamically.Hello, Is.EqualTo("World-"));
+        Assert.That(statically.Hello, Is.EqualTo("World-"));
 
-            var ex1 = Assert.Throws<RuntimeBinderException>(() => alpha.Hello1());
+        Assert.That(dynamically.Foo, Is.EqualTo("Bar-"));
 
-            Assert.That(ex1.Message, Does.EndWith("does not contain a definition for 'Hello1'"));
-
-            var ex2 = Assert.Throws<RuntimeBinderException>(() => beta.Hello2());
-
-            Assert.That(ex2.Message, Does.EndWith("does not contain a definition for 'Hello2'"));
-
-            Assert.That(beta.Sample(), Is.EqualTo("Data"));
-
-        }
-
-
-        [Test]
-        public void GetMemberThrowsFallbackException()
-        {
-            dynamic alpha = new Object();
-            dynamic beta = new Clay(new TestMemberBehavior());
-
-            var ex1 = Assert.Throws<RuntimeBinderException>(() => { var hi = alpha.Hello1; });
-
-            Assert.That(ex1.Message, Does.EndWith("does not contain a definition for 'Hello1'"));
-
-            var ex2 = Assert.Throws<RuntimeBinderException>(() => { var hi = beta.Hello2; });
-
-            Assert.That(ex2.Message, Does.EndWith("does not contain a definition for 'Hello2'"));
-
-            Assert.That(beta.Sample, Is.EqualTo("Data"));
-        }
-
-        [Test]
-        public void SetMemberThrowsFallbackException()
-        {
-            dynamic alpha = new Object();
-            dynamic beta = new Clay(new TestMemberBehavior());
-
-            var ex1 = Assert.Throws<RuntimeBinderException>(() => { alpha.Hello1 = 1; });
-
-            Assert.That(ex1.Message, Does.EndWith("does not contain a definition for 'Hello1'"));
-
-            var ex2 = Assert.Throws<RuntimeBinderException>(() => { beta.Hello2 = 2; });
-
-            Assert.That(ex2.Message, Does.EndWith("does not contain a definition for 'Hello2'"));
-
-            var x = (beta.Sample = 3);
-            Assert.That(x, Is.EqualTo("Data"));
-        }
-
-
-        [Test]
-        public void GetIndexThrowsFallbackException()
-        {
-            dynamic alpha = new Object();
-            dynamic beta = new Clay(new TestMemberBehavior());
-
-            var ex1 = Assert.Throws<RuntimeBinderException>(() => { var hi = alpha[0]; });
-            Assert.That(ex1.Message, Does.Match(@"Cannot apply indexing with \[\] to an expression of type .*"));
-
-            var ex2 = Assert.Throws<RuntimeBinderException>(() => { var hi = beta[0]; });
-            Assert.That(ex2.Message, Does.Match(@"Cannot apply indexing with \[\] to an expression of type .*"));
-        }
-
-
-        [Test]
-        public void SetIndexThrowsFallbackException()
-        {
-            dynamic alpha = new Object();
-            dynamic beta = new Clay(new TestMemberBehavior());
-
-            var ex1 = Assert.Throws<RuntimeBinderException>(() => { alpha[0] = 1; });
-
-            Assert.That(ex1.Message, Does.Match(@"Cannot apply indexing with \[\] to an expression of type .*"));
-
-            var ex2 = Assert.Throws<RuntimeBinderException>(() => { beta[0] = 2; });
-
-            Assert.That(ex2.Message, Does.Match(@"Cannot apply indexing with \[\] to an expression of type .*"));
-
-        }
-
-        public interface IAlpha
-        {
-            string Hello();
-            string Foo();
-        }
-        public class Alpha
-        {
-            public virtual string Hello()
-            {
-                return "World";
-            }
-        }
-
-        public class AlphaBehavior : ClayBehavior
-        {
-            public override object InvokeMember(Func<object> proceed, object self, string name, INamedEnumerable<object> args)
-            {
-                return proceed() + "-";
-            }
-            public override object InvokeMemberMissing(Func<object> proceed, object self, string name, INamedEnumerable<object> args)
-            {
-                if (name == "Foo")
-                    return "Bar";
-                return proceed();
-            }
-        }
-
-        [Test]
-        public void TestInvokePaths()
-        {
-            var dynamically = ClayActivator.CreateInstance<Alpha>(new IClayBehavior[] {
-                new InterfaceProxyBehavior(),
-                new AlphaBehavior()
-            });
-            Alpha statically = dynamically;
-            IAlpha interfacially = dynamically;
-
-            Assert.That(dynamically.Hello(), Is.EqualTo("World-"));
-            Assert.That(statically.Hello(), Is.EqualTo("World-"));
-            Assert.That(interfacially.Hello(), Is.EqualTo("World-"));
-
-            Assert.That(dynamically.Foo(), Is.EqualTo("Bar-"));
-            Assert.That(interfacially.Foo(), Is.EqualTo("Bar-"));
-
-            Assert.Throws<RuntimeBinderException>(() => dynamically.MissingNotHandled());
-        }
-
-
-        public class Beta
-        {
-            public virtual string Hello
-            {
-                get { return "World"; }
-            }
-        }
-
-        public class BetaBehavior : ClayBehavior
-        {
-            public override object GetMember(Func<object> proceed, object self, string name)
-            {
-                return proceed() + "-";
-            }
-            public override object GetMemberMissing(Func<object> proceed, object self, string name)
-            {
-                if (name == "Foo")
-                    return "Bar";
-                return proceed();
-            }
-        }
-
-        [Test]
-        public void TestGetPaths()
-        {
-            var dynamically = ClayActivator.CreateInstance<Beta>(new[] { new BetaBehavior() });
-            Beta statically = dynamically;
-
-            Assert.That(dynamically.Hello, Is.EqualTo("World-"));
-            Assert.That(statically.Hello, Is.EqualTo("World-"));
-
-            Assert.That(dynamically.Foo, Is.EqualTo("Bar-"));
-
-            Assert.Throws<RuntimeBinderException>(() => { var x = dynamically.MissingPropNotHandled; });
-        }
+        Assert.Throws<RuntimeBinderException>(() => { var x = dynamically.MissingPropNotHandled; });
     }
 }
--- src/ClaySharp.Tests/DefaultClayActivatorTests.cs
+++ src/ClaySharp.Tests/DefaultClayActivatorTests.cs
@@ -1,185 +1,179 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using ClaySharp.Behaviors;
+﻿using ClaySharp.Behaviors;
 using NUnit.Framework;
 
-namespace ClaySharp.Tests
+namespace ClaySharp.Tests;
+
+[TestFixture]
+public class DefaultClayActivatorTests
 {
-    [TestFixture]
-    public class DefaultClayActivatorTests
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    private IClayActivator _activator;
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
+    [SetUp]
+    public void Init()
     {
-        private IClayActivator _activator;
+        _activator = new DefaultClayActivator();
+        ClayActivator.ServiceLocator = () => _activator;
+    }
 
-        [SetUp]
-        public void Init()
+    [Test]
+    public void SimpleActivationUsesDefaultClass()
+    {
+        var alpha = ClayActivator.CreateInstance(Enumerable.Empty<IClayBehavior>());
+        var type = alpha.GetType();
+
+        Assert.That(type, Is.EqualTo(typeof(Clay)));
+    }
+
+    public class ClayPlus : Clay
+    {
+        public ClayPlus(IEnumerable<IClayBehavior> behaviors)
+            : base(behaviors)
         {
-            _activator = new DefaultClayActivator();
-            ClayActivator.ServiceLocator = () => _activator;
         }
 
-        [Test]
-        public void SimpleActivationUsesDefaultClass()
-        {
-            var alpha = ClayActivator.CreateInstance(Enumerable.Empty<IClayBehavior>());
-            var type = alpha.GetType();
+        public virtual string Hello { get { return "World"; } }
 
-            Assert.That(type, Is.EqualTo(typeof(Clay)));
+        public virtual int Add(int left, int right)
+        {
+            return left + right;
+        }
+    }
+
+    public interface IClayPlus
+    {
+        string Hello { get; }
+        int Add(int left, int right);
+    }
+
+    [Test]
+    public void ClaySubclassIsActivatedWithoutDynamicProxy()
+    {
+        var alpha = ClayActivator.CreateInstance<ClayPlus>(Enumerable.Empty<IClayBehavior>());
+        var type = alpha.GetType();
+
+        Assert.That(type, Is.EqualTo(typeof(ClayPlus)));
+    }
+
+    [Test]
+    public void SubclassMembersRemainAvailableStaticallyAndDynamicallyAndViaInterface()
+    {
+
+        var alpha = ClayActivator.CreateInstance<ClayPlus>(new[] { new InterfaceProxyBehavior() });
+
+        dynamic dynamically = alpha;
+        ClayPlus statically = alpha;
+        IClayPlus interfacially = alpha;
+
+        Assert.That(dynamically.Hello, Is.EqualTo("World"));
+        Assert.That(statically.Hello, Is.EqualTo("World"));
+        Assert.That(interfacially.Hello, Is.EqualTo("World"));
+
+        Assert.That(dynamically.Add(3, 4), Is.EqualTo(7));
+        Assert.That(statically.Add(3, 4), Is.EqualTo(7));
+        Assert.That(interfacially.Add(3, 4), Is.EqualTo(7));
+        Assert.That(interfacially.Add(3, 5), Is.EqualTo(8));
+        Assert.That(interfacially.Add(3, 6), Is.EqualTo(9));
+    }
+
+    public class Anything
+    {
+        private readonly string? _helloText;
+
+        public Anything()
+        {
+            _helloText = null;
         }
 
-        public class ClayPlus : Clay
+        public Anything(string helloText)
         {
-            public ClayPlus(IEnumerable<IClayBehavior> behaviors)
-                : base(behaviors)
+            _helloText = helloText;
+        }
+
+        public virtual string Hello { get { return _helloText ?? "World"; } }
+
+        public virtual int Add(int left, int right)
+        {
+            return left + right;
+        }
+    }
+
+    [Test]
+    public void ClaySubclassFromAnythingIsActivatedDynamixProxyAddingDlrInterfaces()
+    {
+        var alpha = ClayActivator.CreateInstance<Anything>(Enumerable.Empty<IClayBehavior>());
+
+        var type = alpha.GetType();
+
+        Assert.That(type, Is.Not.EqualTo(typeof(Anything)));
+        Assert.That(typeof(Anything).IsAssignableFrom(type));
+    }
+
+    [Test]
+    public void SubclassFromAnythingMembersRemainAvailableStaticallyAndDynamicallyAndViaInterface()
+    {
+        var alpha = ClayActivator.CreateInstance<Anything>(new[] { new InterfaceProxyBehavior() });
+        var type = alpha.GetType();
+        Assert.That(type, Is.Not.EqualTo(typeof(Anything)));
+
+        dynamic dynamically = alpha;
+        Anything statically = alpha;
+        IClayPlus interfacially = alpha;
+
+        Assert.That(dynamically.Hello, Is.EqualTo("World"));
+        Assert.That(statically.Hello, Is.EqualTo("World"));
+        Assert.That(interfacially.Hello, Is.EqualTo("World"));
+
+        Assert.That(dynamically.Add(3, 4), Is.EqualTo(7));
+        Assert.That(statically.Add(3, 4), Is.EqualTo(7));
+        Assert.That(interfacially.Add(3, 4), Is.EqualTo(7));
+        Assert.That(interfacially.Add(3, 5), Is.EqualTo(8));
+        Assert.That(interfacially.Add(3, 6), Is.EqualTo(9));
+    }
+
+
+    [Test]
+    public void BehaviorsCanFilterVirtualMethods()
+    {
+        var alpha = ClayActivator.CreateInstance<Anything>(new IClayBehavior[] {
+            new InterfaceProxyBehavior(),
+            new AnythingModifier() });
+
+        dynamic dynamically = alpha;
+        Anything statically = alpha;
+        IClayPlus interfacially = alpha;
+
+        Assert.That(dynamically.Hello, Is.EqualTo("[World]"));
+        Assert.That(statically.Hello, Is.EqualTo("[World]"));
+        Assert.That(interfacially.Hello, Is.EqualTo("[World]"));
+
+        Assert.That(dynamically.Add(3, 4), Is.EqualTo(9));
+        Assert.That(statically.Add(3, 4), Is.EqualTo(9));
+        Assert.That(interfacially.Add(3, 4), Is.EqualTo(9));
+        Assert.That(interfacially.Add(3, 5), Is.EqualTo(10));
+        Assert.That(interfacially.Add(3, 6), Is.EqualTo(11));
+    }
+
+    class AnythingModifier : ClayBehavior
+    {
+        public override object InvokeMember(Func<object> proceed, object self, string name, INamedEnumerable<object> args)
+        {
+            if (name == "Add")
             {
+                return (int)proceed() + 2;
             }
 
-            public virtual string Hello { get { return "World"; } }
-
-            public virtual int Add(int left, int right)
+            return proceed();
+        }
+        public override object GetMember(Func<object> proceed, object self, string name)
+        {
+            if (name == "Hello")
             {
-                return left + right;
-            }
-        }
-
-        public interface IClayPlus
-        {
-            string Hello { get; }
-            int Add(int left, int right);
-        }
-
-        [Test]
-        public void ClaySubclassIsActivatedWithoutDynamicProxy()
-        {
-            var alpha = ClayActivator.CreateInstance<ClayPlus>(Enumerable.Empty<IClayBehavior>());
-            var type = alpha.GetType();
-
-            Assert.That(type, Is.EqualTo(typeof(ClayPlus)));
-        }
-
-        [Test]
-        public void SubclassMembersRemainAvailableStaticallyAndDynamicallyAndViaInterface()
-        {
-
-            var alpha = ClayActivator.CreateInstance<ClayPlus>(new[] { new InterfaceProxyBehavior() });
-
-            dynamic dynamically = alpha;
-            ClayPlus statically = alpha;
-            IClayPlus interfacially = alpha;
-
-            Assert.That(dynamically.Hello, Is.EqualTo("World"));
-            Assert.That(statically.Hello, Is.EqualTo("World"));
-            Assert.That(interfacially.Hello, Is.EqualTo("World"));
-
-            Assert.That(dynamically.Add(3, 4), Is.EqualTo(7));
-            Assert.That(statically.Add(3, 4), Is.EqualTo(7));
-            Assert.That(interfacially.Add(3, 4), Is.EqualTo(7));
-            Assert.That(interfacially.Add(3, 5), Is.EqualTo(8));
-            Assert.That(interfacially.Add(3, 6), Is.EqualTo(9));
-        }
-
-
-        public class Anything
-        {
-            private readonly string _helloText;
-
-            public Anything()
-            {
-            }
-            public Anything(string helloText)
-            {
-                _helloText = helloText;
+                return $"[{proceed()}]";
             }
 
-            public virtual string Hello { get { return _helloText ?? "World"; } }
-
-            public virtual int Add(int left, int right)
-            {
-                return left + right;
-            }
+            return proceed();
         }
-
-        [Test]
-        public void ClaySubclassFromAnythingIsActivatedDynamixProxyAddingDlrInterfaces()
-        {
-            var alpha = ClayActivator.CreateInstance<Anything>(Enumerable.Empty<IClayBehavior>());
-
-            var type = alpha.GetType();
-
-            Assert.That(type, Is.Not.EqualTo(typeof(Anything)));
-            Assert.That(typeof(Anything).IsAssignableFrom(type));
-
-        }
-
-        [Test]
-        public void SubclassFromAnythingMembersRemainAvailableStaticallyAndDynamicallyAndViaInterface()
-        {
-            var alpha = ClayActivator.CreateInstance<Anything>(new[] { new InterfaceProxyBehavior() });
-            var type = alpha.GetType();
-            Assert.That(type, Is.Not.EqualTo(typeof(Anything)));
-
-            dynamic dynamically = alpha;
-            Anything statically = alpha;
-            IClayPlus interfacially = alpha;
-
-            Assert.That(dynamically.Hello, Is.EqualTo("World"));
-            Assert.That(statically.Hello, Is.EqualTo("World"));
-            Assert.That(interfacially.Hello, Is.EqualTo("World"));
-
-            Assert.That(dynamically.Add(3, 4), Is.EqualTo(7));
-            Assert.That(statically.Add(3, 4), Is.EqualTo(7));
-            Assert.That(interfacially.Add(3, 4), Is.EqualTo(7));
-            Assert.That(interfacially.Add(3, 5), Is.EqualTo(8));
-            Assert.That(interfacially.Add(3, 6), Is.EqualTo(9));
-        }
-
-
-        [Test]
-        public void BehaviorsCanFilterVirtualMethods()
-        {
-
-            var alpha = ClayActivator.CreateInstance<Anything>(new IClayBehavior[] {
-                new InterfaceProxyBehavior(),
-                new AnythingModifier() });
-
-            dynamic dynamically = alpha;
-            Anything statically = alpha;
-            IClayPlus interfacially = alpha;
-
-            Assert.That(dynamically.Hello, Is.EqualTo("[World]"));
-            Assert.That(statically.Hello, Is.EqualTo("[World]"));
-            Assert.That(interfacially.Hello, Is.EqualTo("[World]"));
-
-            Assert.That(dynamically.Add(3, 4), Is.EqualTo(9));
-            Assert.That(statically.Add(3, 4), Is.EqualTo(9));
-            Assert.That(interfacially.Add(3, 4), Is.EqualTo(9));
-            Assert.That(interfacially.Add(3, 5), Is.EqualTo(10));
-            Assert.That(interfacially.Add(3, 6), Is.EqualTo(11));
-        }
-
-        class AnythingModifier : ClayBehavior
-        {
-            public override object InvokeMember(Func<object> proceed, object self, string name, INamedEnumerable<object> args)
-            {
-                if (name == "Add")
-                {
-                    return (int)proceed() + 2;
-                }
-                return proceed();
-            }
-            public override object GetMember(Func<object> proceed, object self, string name)
-            {
-                if (name == "Hello")
-                {
-                    return "[" + proceed() + "]";
-                }
-
-                return proceed();
-            }
-
-        }
-
-
     }
 }
